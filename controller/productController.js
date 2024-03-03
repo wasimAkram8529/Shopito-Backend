1d8c93b4583009c73c69fa0520c0124f279b8b13
wasimAkram8529 <codelikearocstar@gmail.com>
2024-03-03 18:26:59 +0530

    product added

--- controller/productController.js
+++ controller/productController.js
@@ -141,11 +141,11 @@ const getProducts = asyncHandler(async (req, res) => {
     const fieldsList = fields.split(",").join(" ");
     result = result.select(fieldsList);
   }
-  const page = Number(req.query.page) || 1;
-  const limit = Number(req.query.limit) || 10;
-  const skip = (page - 1) * limit;
+  // const page = Number(req.query.page) || 1;
+  // const limit = Number(req.query.limit) || 10;
+  // const skip = (page - 1) * limit;
 
-  result = result.skip(skip).limit(limit);
+  // result = result.skip(skip).limit(limit);
   // 23
   // 4 7 7 7 2
 
